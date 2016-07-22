8d435c894b6bbe1c985aef58003941cae94d4f0f
Brent Carmer <bcarmer@gmail.com>
2016-07-22 17:59:15 -0400

    Add tribes-based weak prf

---
+++ circuits/tribes.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE Strict #-}
+
+import Circuit
+import Circuit.Builder
+import qualified Circuit.Format.Acirc as Acirc
+import Util
+
+import Control.Monad
+import Data.List.Split
+import Debug.Trace
+import qualified Data.Vector as V
+
+matrixMul :: [[Ref]] -> [Ref] -> Builder [Ref]
+matrixMul rows vect
+  | not $ all ((== length vect) . length) rows = error "[matrixMul] bad dimensions"
+  | otherwise = mapM (circXors <=< zipWithM circMul vect) rows
+
+tribes :: Int -> [Ref] -> Ref -> Builder Ref
+tribes k y z = circXor z =<< circOrs =<< mapM circProd (chunksOf k y)
+
+fa :: Int -> Int -> IO Circuit
+fa n k = do
+    keyBits <- randKeyIO ((n+1)^2)
+    return $ buildCircuit $ do
+        a <- chunksOf (n+1) <$> secrets keyBits
+        x <- inputs (n+1)
+        w <- matrixMul a x
+        z <- tribes k (init w) (last w)
+        output z
+
+fa_128 :: IO Circuit
+fa_128 = fa 128 4
