c3942f47216085be7dc38cc16ed29674fb0387bf
Alex J. Malozemoff <amaloz@galois.com>
2017-04-17 17:05:36 -0700

    continue cleanup of generate-circuits script

--- scripts/generate-circuits.sh
+++ scripts/generate-circuits.sh
@@ -25,26 +25,30 @@ function generate_circuit() {
 
         C2V)
             result_file=$3.c2v.acirc
-            "$scriptdir"/c2v $source_file $func_name > $result_file
+            "$scriptdir"/c2v "$source_file" "$func_name" > "$result_file"
             ;;
 
         *)
-            echo "[error] unrecognized compilation mode:" $type_of_compilation
+            echo "[error] unrecognized compilation mode:" "$type_of_compilation"
             exit 1
             ;;
     esac
-    add_tests $result_file
+    add_tests "$result_file"
 }
 
-# generate AES circuits
+#
+# Generate AES circuits
+#
 for ty in C2A C2V; do
-    for f in aes1r b0 b0_64 b0_32 b0_16 b0_8 b0_7 b0_6 b0_5 b0_4 b0_3 b0_2 sbox_ linearParts; do
+    for f in aes1r b0 b0_64 b0_32 b0_16 b0_8 b0_4 b0_2 sbox_ linearParts; do
         generate_circuit $ty "$cryptoldir"/AES.cry $f
     done
 done
 cabal run --verbose=0 -- -C aes # requires linearParts
 
-# generate goldreich prg circuits
+#
+# Generate Goldreich PRG circuits
+#
 for ty in C2A C2V; do
     for f in prg_16_16 prg_16_32 prg_16_48 prg_16_64 prg_32_32 prg_32_64 prg_32_96; do
         generate_circuit $ty "$cryptoldir"/goldreich.cry $f
@@ -52,46 +56,58 @@ for ty in C2A C2V; do
 done
 cabal run --verbose=0 -- -C goldreich
 
+#
 # generate GGM circuits
+#
 cabal run --verbose=0 -- -C ggm
 
-# generate Applebaum-Raykov circuits
+#
+# Generate Applebaum-Raykov circuits
+#
 for ty in C2A C2V; do
     for f in f1_16 f1_32 f1_64 f1_128 f3_4; do
         generate_circuit $ty "$cryptoldir"/applebaum.cry $f
     done
-done
-
-for ty in C2A C2V; do
     for f in mapper_2 mapper_4 mapper_8; do
         generate_circuit $ty "$cryptoldir"/mapper.cry $f
     done
 done
-
 # ensure the mapper exists for f3_4
 if [ ! -e mappers/mapper_8 ]; then
     mkdir -p mappers
-    generate_circuit C2V "$cryptoldir"/mapper.cry mapper_8
-    mv mapper_8.c2v.acirc mappers
+    # generate_circuit C2V "$cryptoldir"/mapper.cry mapper_8
+    cp mapper_8.c2v.acirc mappers
 fi
-
 cabal run --verbose=0 -- -C applebaum
 
-# generate tribes circuits
-cabal run --verbose=0 -- -C tribes
+#
+# Generate tribes circuits
+#
+# cabal run --verbose=0 -- -C tribes
 
-# generate mimc circuits
-for ty in C2A C2V; do
-    for f in mimc_16_10r mimc_16_5r mimc_16_1r mimc_8_5r mimc_8_1r; do
-        generate_circuit $ty "$cryptoldir"/mimc.cry $f
-    done
-done
+#
+# Generate MIMC circuits
+#
+# for ty in C2A C2V; do
+#     for f in mimc_16_10r mimc_16_5r mimc_16_1r mimc_8_5r mimc_8_1r; do
+#         generate_circuit $ty "$cryptoldir"/mimc.cry $f
+#     done
+# done
 
+#
+# Generate optimized-DSL circuits
+#
 for c in ./*.dsl.acirc; do
-    cabal run --verbose=0 -- -O2 $c -o ${c/dsl/opt}
+    # skip ones that take forever
+    if [ "$c" == aesr1.dsl.acirc ]; then
+        continue
+    fi
+    cabal run --verbose=0 -- -O2 "$c" -o "${c/dsl/opt}"
 done
 
-# package everything
+#
+# Package everything
+#
 tmpdir=$(mktemp -d)
 mkdir "$tmpdir"/other "$tmpdir"/sigma
 mv ./*.acirc "$tmpdir"
