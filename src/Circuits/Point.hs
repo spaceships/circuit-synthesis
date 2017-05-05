c6190f52cbfb470e648a1544e512980d2775c1e6
Brent Carmer <bcarmer@gmail.com>
2017-05-05 16:01:31 -0700

    Actually add the new source file

---
+++ src/Circuits/Point.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE TupleSections #-}
+
+module Circuits.Point where
+
+import Circuit
+import Circuit.Builder
+-- import qualified Circuit.Format.Acirc as Acirc
+import Data.List.Split (chunksOf)
+import Control.Monad
+import Util
+import Rand
+
+make :: IO [(Maybe String, Circuit)]
+make = sequence
+    [ (Just "point.dsl.acirc",) <$> point 27 8
+    ]
+
+point :: Int -> Int -> IO Circuit
+point ninputs symlen = do
+    let q = (fromIntegral ninputs :: Integer) ^ (fromIntegral symlen :: Integer)
+    thePoint <- randIntegerModIO q
+    let nbits = numBits q
+        bs    = num2Bits nbits thePoint
+        ixs   = map bits2Num $ chunksOf (nbits `div` ninputs) bs
+        sels  = map (toSel symlen) ixs
+    return $ buildCircuit $ do
+        setSymlen symlen
+        xs <- replicateM ninputs (inputs symlen)
+        ys <- mapM secrets sels
+        zs <- mapM circSum =<< mapM (uncurry (zipWithM circMul)) (zip xs ys)
+        output =<< circProd zs
+  where
+    toSel n x = [ if i == x then 1 else 0 | i <- [0..n-1] ]
