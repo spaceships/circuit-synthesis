ea3256487d7b7db7a1c2c1858e67dfd7290d8176
Brent Carmer <carmerb@eecs.oregonstate.edu>
2017-05-08 15:06:03 -0700

    Fix comparison

--- src/Circuits/Comparison.hs
+++ src/Circuits/Comparison.hs
@@ -7,7 +7,7 @@ import Circuit.Builder
 import Control.Monad
 
 make :: IO [(Maybe String, Circuit)]
-make = return [ (Just "comparison.dsl.acirc", comparison 27 8) ]
+make = return [ (Just "comparison.dsl.acirc", comparison 22 4) ]
 
 -- 10^12 input size
 -- racheled as 22 vectors of length 4
@@ -17,8 +17,7 @@ comparison nsyms symlen = buildCircuit $ do
     setSymlen symlen
     xs <- replicateM nsyms (inputs symlen)
     ys <- replicateM nsyms (inputs symlen) -- assumed to come in the negated sigma vector form
-    zs <- zipWithM squash xs ys
-    z  <- circSum zs -- since we zero check at the end, we dont care if z > 1. We dont have to maintain the boolean emulation here.
+    z  <- circSum =<< zipWithM squash xs ys
     output z
   where
-    squash as bs = circProd =<< zipWithM circMul as bs
+    squash as bs = circSum =<< zipWithM circMul as bs
